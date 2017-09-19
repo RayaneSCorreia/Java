75958a8fd2e63a8b4ca7fc7d213904593a269337
Rayane3229 <32071072+Rayane3229@users.noreply.github.com>
2017-09-19 08:59:46 -0300

    Update Contato.java
    Classe contato para a criação de lista em usuário. Previamente adicionado como favorito para ser enviada a localização do usuário via SMS. Contato como extensão de Pessoa.

--- Contato.java
+++ Contato.java
@@ -4,10 +4,4 @@ package tsaveprojectpoo;
 public class Contato extends Pessoa {
 	
 	boolean favorito;
-
-	// public void obterInfo(Contato contato){
-	// JOptionPane.showMessageDialog(null, contato.nome + "\n " +
-	// contato.telefone + "\n");}
-
-
 }
