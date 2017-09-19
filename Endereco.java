67936c64158c4d89df7cc041843ce90f2c0780d9
Rayane3229 <32071072+Rayane3229@users.noreply.github.com>
2017-09-19 09:10:23 -0300

    Update Endereco.java
    Classe Endereço criada para especificação e para demonstração de sobrecarga, onde se tem dois construtores com o mesmo nome, porém com parâmetros diferentes.

--- Endereco.java
+++ Endereco.java
@@ -1,13 +1,12 @@
 package tsaveprojectpoo;
 
-
 public class Endereco {
 	
 	String rua;
 	String numero;
 	String complemento;
 
-	public Endereco(String rua, String numero, String complemento) {
+	public Endereco (String rua, String numero, String complemento) {
 		this.rua = rua;
 		this.numero = numero;
 		this.complemento = complemento;
